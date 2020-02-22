2a4cf483562cfe4ef4baa86c06b6853588de402f
Anders Hovmöller <boxed@killingar.net>
2020-02-22 09:23:06 +0100

    2 declarative table

--- Supernaut/Supernaut/urls.py
+++ Supernaut/Supernaut/urls.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.urls import path
 
+from iommi import (
+    Table,
+    Column,
+)
+
+from .models import Album
 from .example_data import setup_example_data
 
 setup_example_data()
+
+
+# Tables ---------------------------
+
+class AlbumsTable(Table):
+    name = Column()
+    artist = Column()
+    year = Column()
 
 
 # Views ----------------------------
@@ -13,7 +27,9 @@ def index(request):
         request,
         template_name='index.html',
         context=dict(
-            content='hello',
+            content=AlbumsTable(
+                rows=Album.objects.all(),
+            ).bind(request=request),
             title='Title here!',
         )
     )
