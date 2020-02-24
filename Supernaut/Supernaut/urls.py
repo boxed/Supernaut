fd70ed9e5f7c9ae34b8eb44898be6db535db523c
Anders Hovmöller <anders.hovmoller@trioptima.com>
2020-02-24 10:04:51 +0100

    5 auto__model

--- Supernaut/Supernaut/urls.py
+++ Supernaut/Supernaut/urls.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
 from django.urls import path
 
 from iommi import (
     Table,
-    Column,
 )
 
 from .models import Album
@@ -11,21 +9,8 @@ from .example_data import setup_example_data
 
 setup_example_data()
 
-
-# Tables ---------------------------
-
-class AlbumsTable(Table):
-    name = Column()
-    artist = Column()
-    year = Column()
-
-    class Meta:
-        title = 'Albums'
-        rows = Album.objects.all()
-
-
 # URLs -----------------------------
 
 urlpatterns = [
-    path('', AlbumsTable().as_view()),
+    path('', Table(auto__model=Album).as_view()),
 ]
